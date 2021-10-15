522d3d0842dbd25072dde983c858d78f375bb464
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2021-10-15 19:37:31 +0200

    Add files via upload

---
+++ Objekti i Klase 20210321/Objekti i Klase 20210321/Vozilo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objekti_i_Klase_20210321
+{
+    class Vozilo
+    {
+        bool JeLiLeti, JeLiPlovi;
+
+        public bool JeLiLeti1 { get => JeLiLeti; set => JeLiLeti = value; }
+        public bool JeLiPlovi1 { get => JeLiPlovi; set => JeLiPlovi = value; }
+
+        public void KudaVozi()
+        {
+            Console.WriteLine("Leti = " + JeLiLeti + "Plovi = " + JeLiPlovi);
+        }
+
+        public Vozilo(bool jeLiLeti, bool jeLiPlovi)
+        {
+            JeLiLeti = jeLiLeti;
+            JeLiPlovi = jeLiPlovi;
+        }
+    }
+}
